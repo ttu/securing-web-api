d9f5cbcab48fa7c48bf284c3523a2329559230dd
ttu <tomi.tuhkanen@iki.fi>
2024-06-20 18:35:20 +0200

    Refactor API get service functions

--- api/src/db.ts
+++ api/src/db.ts
@@ -6,19 +6,20 @@ const USERS: UserInfo[] = [
   { id: 3, name: 'Charlie', age: 28 },
 ];
 
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 5000));
+
 export const getUsers = async () => {
   // const users = await db.query("SELECT * FROM users");
-  console.log('DB - Returning users from db');
-  // Simulate slow query by sleeping for 5 seconds
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log('DB - Returning users from db with slow query');
+  await sleep();
+
   return Promise.resolve(USERS);
 };
 
 export const getUser = async (id: number) => {
   // const user = await db.query("SELECT * FROM users WHERE id = $1", [id]);
-  console.log('DB - Returning user from db');
-  // Simulate slow query by sleeping for 5 seconds
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log('DB - Returning user from db with slow query');
+  await sleep();
 
   const user = USERS.find((user) => user.id === id);
   return Promise.resolve(user);
--- api/src/service.ts
+++ api/src/service.ts
@@ -2,6 +2,54 @@ import { UserInfo } from './types';
 import * as db from './db';
 import * as cache from './cache';
 
+export const getUsers = async (): Promise<UserInfo[]> => getUsersCache();
+
+export const getUser = async (id: number): Promise<UserInfo | undefined> => getUserCache(id);
+
+const getUsersNoCache = async (): Promise<UserInfo[]> => {
+  const users = await db.getUsers();
+  return users;
+};
+
+const getUserNoCache = async (id: number): Promise<UserInfo | undefined> => {
+  const user = await db.getUser(id);
+  return user;
+};
+
+const getUsersCache = async (): Promise<UserInfo[]> => {
+  const users = await cacheWrapper('users', () => db.getUsers());
+  return users;
+};
+
+const getUserCache = async (id: number): Promise<UserInfo | undefined> => {
+  const user = await cacheWrapper(`user_${id}`, () => db.getUser(id));
+  return user;
+};
+
+const getUsersWithoutCacheWrapper = async (): Promise<UserInfo[]> => {
+  const cacheKey = `users`;
+  const cachedUsers = await cache.get<UserInfo[]>(cacheKey);
+  if (cachedUsers) {
+    console.log('Cache hit');
+    return cachedUsers;
+  }
+  const users = await db.getUsers();
+  cache.add(cacheKey, users);
+  return users;
+};
+
+const getUserWithoutCacheWrapper = async (id: number): Promise<UserInfo | undefined> => {
+  const cacheKey = `user_${id}`;
+  const cachedUser = await cache.get<UserInfo>(cacheKey);
+  if (cachedUser) {
+    console.log('Cache hit');
+    return cachedUser;
+  }
+  const user = await db.getUser(id);
+  cache.add(cacheKey, user);
+  return user;
+};
+
 const cacheWrapper = async <T>(cacheKey: string, getData: () => Promise<T>) => {
   const cached = await cache.get<T>(cacheKey);
   if (cached) {
@@ -12,28 +60,4 @@ const cacheWrapper = async <T>(cacheKey: string, getData: () => Promise<T>) => {
   const data = await getData();
   await cache.add(cacheKey, data);
   return data;
-};
-
-export const getUsers = async (): Promise<UserInfo[]> => {
-  // const users = await db.getUsers();
-  const users = await cacheWrapper('users', () => db.getUsers());
-  return users;
-};
-
-export const getUser = async (id: number): Promise<UserInfo | undefined> => {
-  // const user = await db.getUser(id);
-  const user = await cacheWrapper(`user_${id}`, () => db.getUser(id));
-  return user;
-};
-
-export const getUserWithoutCacheWrapper = async (id: number): Promise<UserInfo | undefined> => {
-  const cacheKey = `user_${id}`;
-  const cachedUser = cache.get<UserInfo>(cacheKey);
-  if (cachedUser) {
-    console.log('Cache hit');
-    return cachedUser;
-  }
-  const user = await db.getUser(id);
-  cache.add(cacheKey, user);
-  return user;
 };
