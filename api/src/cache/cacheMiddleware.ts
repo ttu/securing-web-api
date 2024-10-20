12f39775afd5e1743c6eb6fe7e43743adb394423
ttu <tomi.tuhkanen@iki.fi>
2024-10-20 22:10:15 +0300

    Add route specifc cache middleware

--- api/src/cache/cacheMiddleware.ts
+++ api/src/cache/cacheMiddleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 import * as cache from './cache';
 
+// Application wide cache middleware
 export const cacheMiddleware = (durationInSec: number) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     // Skip caching for authenticated routes and POST requests
@@ -12,6 +13,39 @@ export const cacheMiddleware = (durationInSec: number) => {
     }
 
     const key = `__express__${req.originalUrl || req.url}`;
+    const cacheContent = await cache.get(key);
+    if (cacheContent) {
+      console.log('Cache hit from middleware: key', key);
+      // NOTE: For now we just assume that the cached content is JSON
+      res.setHeader('Content-Type', 'application/json');
+      res.send(cacheContent);
+      return;
+    }
+
+    // Save the original res.send method with proper context
+    const originalSend = res.send.bind(res);
+
+    // @ts-expect-error res.send would require more complex typing
+    res.send = async (body: unknown) => {
+      // Cache only successful responses
+      if (res.statusCode < 400) {
+        await cache.add(key, body, durationInSec);
+      }
+
+      originalSend(body); // Call the original res.send method
+    };
+
+    next();
+  };
+};
+
+// Cache middleware for route level caching
+// NOTE: Cache should have an optional cache key as we might need to invalidate cache in some cases
+export const routeCache = (durationInSec: number) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const prefix = `__express__${req.originalUrl || req.url}`;
+    const key = req.userId ? `${prefix}__user__${req.userId}` : prefix;
+
     const cacheContent = await cache.get(key);
     if (cacheContent) {
       console.log('Cache hit from middleware: key', key);
--- api/src/features/orders/routes.ts
+++ api/src/features/orders/routes.ts
@@ -2,10 +2,13 @@ import { Request, Response, Router } from 'express';
 
 import { createOrder, getOrders } from './service';
 import { hasValidOrderData, isValidOrderDto } from './types';
+import { routeCache } from '../../cache/cacheMiddleware';
 import { authMiddleware } from '../../middlewares/authMiddleware';
 
 export const router = Router();
 
+// Endpoint can have cache in route or in service
+// router.get('/', authMiddleware, routeCache(10), async (req: Request, res: Response) => {
 router.get('/', authMiddleware, async (req: Request, res: Response) => {
   const userId = req.userId!;
   const orders = await getOrders(userId);
